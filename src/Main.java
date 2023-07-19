48bf17b56bdc6fbd49c8838399dc139f53becefd
Vel <korzhavin.vel@yandex.ru>
2023-07-19 18:21:33 +0700

    String homeWork task1,2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+//        task1();
+        task2();
     }
+
+//    public static void task1() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Задача 1");
+//        System.out.println("Введите имя:");
+//        String firstName = scanner.next();
+//        System.out.println("Введите отчество:");
+//        String middleName = scanner.next();
+//        System.out.println("Введите фамилию:");
+//        String lastName = scanner.next();
+//        String fullName = lastName + " " + firstName + " " + middleName;
+//        System.out.println("ФИО сотрудника - " + fullName);
+//    }
+
+    public static void task2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Задача 2");
+        System.out.println("Введите имя:");
+        String firstName = scanner.next();
+        System.out.println("Введите отчество:");
+        String middleName = scanner.next();
+        System.out.println("Введите фамилию:");
+        String lastName = scanner.next();
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
+    }
+
+
 }
